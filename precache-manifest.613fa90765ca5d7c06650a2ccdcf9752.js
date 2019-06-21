633b1ddd376039f7cc5eb86ec6737966d15fbeb7
Frankuy <fhizbullah99@gmail.com>
2019-06-21 17:36:43 +0700

    Updates

---
+++ precache-manifest.613fa90765ca5d7c06650a2ccdcf9752.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81650d6e4bd2c5f26ae5e6a8d715b2e",
+    "url": "/Nim-Finder/index.html"
+  },
+  {
+    "revision": "626e474c8553443857cb",
+    "url": "/Nim-Finder/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "b310ef8b888a21ebe99a",
+    "url": "/Nim-Finder/static/css/main.b8bed287.chunk.css"
+  },
+  {
+    "revision": "626e474c8553443857cb",
+    "url": "/Nim-Finder/static/js/2.00f8ef45.chunk.js"
+  },
+  {
+    "revision": "b310ef8b888a21ebe99a",
+    "url": "/Nim-Finder/static/js/main.111a3b2a.chunk.js"
+  },
+  {
+    "revision": "6a4808a9cb63b3d94d64",
+    "url": "/Nim-Finder/static/js/runtime~main.934d47fc.js"
+  },
+  {
+    "revision": "5aa1d9518fe48401db3f6caf12265bf3",
+    "url": "/Nim-Finder/static/media/logo.5aa1d951.svg"
+  }
+]);
